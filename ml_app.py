5db1dd8fead6eee2d8c530932e33b92f942477d8
jayasimha4226 <127817990+jayasimha4226@users.noreply.github.com>
2024-04-17 11:17:05 +0530

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -1,1 +1,9 @@
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier(
+  model = joblib.load('finalized_model.joblib')
+  # caching the model for faster loading
+  @st.cache
