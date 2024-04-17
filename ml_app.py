d8eca13f9a7192a9b6880d926142c42f55391e7f
jayasimha4226 <127817990+jayasimha4226@users.noreply.github.com>
2024-04-17 11:23:20 +0530

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import joblib
 from sklearn.tree import DecisionTreeClassifier
-model = DecisionTreeClassifier(
-  model = joblib.load('finalized_model.joblib')
+model = DecisionTreeClassifier(criterion='gini', max_depth = 3, random_state=0)
+#clf_gini.fit(X_train, y_train)
+model = joblib.load('finalized_model.joblib')
   # caching the model for faster loading
-  @st.cache
+@st.cache
